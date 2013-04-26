ad73c49b7a4850c5f4e751ff7179fc31090193a7
Travis Keep <keep94@gmail.com>
2013-04-26 05:56:47 -0700

    Add HandlerWithOptions method and a unit test.

--- weblogs.go
+++ weblogs.go
@@ -19,36 +19,38 @@ const (
 )
 
 var (
-  kLog *log.Logger
+  kNilWriter nilWriter
 )
 
-// Handler wraps handler creating access logs. Returned handler must be
+// Options specifies options for writing to access logs.
+type Options struct {
+  // Where to write the web logs. nil means write to stderr,
+  Writer io.Writer
+}
+
+func (o *Options) writer() io.Writer {
+  if o.Writer == nil {
+    return os.Stderr
+  }
+  return o.Writer
+}
+
+// Handler wraps a handler creating access logs. Returned handler must be
 // wrapped by context.ClearHandler.
 func Handler(handler http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    remoteAddr, method, url := r.RemoteAddr, r.Method, r.URL
-    additional := &bytes.Buffer{}
-    context.Set(r, kBufferKey, additional)
-    writer := &statusWriter{ResponseWriter: w}
-    defer func() {
-      err := recover()
-      writer.MaybeSend500()
-      var buf bytes.Buffer
-      if additional.Len() == 0 {
-        fmt.Fprintf(
-            &buf, "%s %s %s %d\n", remoteAddr, method, url, writer.Status())
-      } else {
-        fmt.Fprintf(
-            &buf, "%s %s %s %d%s\n", remoteAddr, method, url, writer.Status(), additional.String())
-      }
-      if err != nil {
-        fmt.Fprintf(&buf, "Panic: %v\n", err)
-        buf.Write(debug.Stack())
-      }
-      kLog.Print(buf.String())
-    }()
-    handler.ServeHTTP(writer, r)
-  })
+  return HandlerWithOptions(handler, nil)
+}
+
+// HandlerWithOptions wraps a handler creating access logs and allows caller to
+// configure how access logs are written. Returned handler must be
+// wrapped by context.ClearHandler.
+func HandlerWithOptions(
+    handler http.Handler, options *Options) http.Handler {
+  if options == nil {
+    options = &Options{}
+  }
+  l := log.New(options.writer(), "", log.LstdFlags | log.Lmicroseconds)
+  return &logHandler{handler: handler, alog: l}
 }
 
 // Writer returns a writer whereby the caller can add additional information
@@ -58,9 +60,39 @@ func Handler(handler http.Handler) http.Handler {
 func Writer(r *http.Request) io.Writer {
   value := context.Get(r, kBufferKey)
   if value == nil {
-    return nilWriter{}
+    return kNilWriter
   }
   return value.(*bytes.Buffer)
+}
+
+type logHandler struct {
+  handler http.Handler
+  alog *log.Logger
+}
+
+func (h *logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  remoteAddr, method, url := r.RemoteAddr, r.Method, r.URL
+  additional := &bytes.Buffer{}
+  context.Set(r, kBufferKey, additional)
+  writer := &statusWriter{ResponseWriter: w}
+  defer func() {
+    err := recover()
+    writer.MaybeSend500()
+    var buf bytes.Buffer
+    if additional.Len() == 0 {
+      fmt.Fprintf(
+          &buf, "%s %s %s %d\n", remoteAddr, method, url, writer.Status())
+    } else {
+      fmt.Fprintf(
+          &buf, "%s %s %s %d%s\n", remoteAddr, method, url, writer.Status(), additional.String())
+    }
+    if err != nil {
+      fmt.Fprintf(&buf, "Panic: %v\n", err)
+      buf.Write(debug.Stack())
+    }
+    h.alog.Print(buf.String())
+  }()
+  h.handler.ServeHTTP(writer, r)
 }
 
 func sendError(w http.ResponseWriter, status int) {
@@ -106,8 +138,4 @@ type nilWriter struct {
 
 func (w nilWriter) Write(p []byte) (n int, err error) {
   return len(p), nil
-}
-
-func init() {
-  kLog = log.New(os.Stderr, "", log.LstdFlags | log.Lmicroseconds)
 }
---
+++ weblogs_test.go
@@ -0,0 +1,115 @@
+package weblogs_test
+
+import (
+  "bytes"
+  "fmt"
+  "github.com/keep94/weblogs"
+  "net/http"
+  "net/url"
+  "strings"
+  "testing"
+)
+
+var (
+  kNilResponseWriter nilResponseWriter
+)
+
+func TestNormalLogs(t *testing.T) {
+  buf := &bytes.Buffer{}
+  handler := weblogs.HandlerWithOptions(
+      &handler{Status: 321}, &weblogs.Options{Writer: buf})
+  handler.ServeHTTP(
+      kNilResponseWriter,
+      newRequest("192.168.5.1", "GET", "/foo/bar?query=tall"))
+  actual := buf.String()
+  expected := "192.168.5.1 GET /foo/bar?query=tall 321\n"
+  verifyLogs(t, expected, actual)
+}
+
+func TestAppendedLogs(t *testing.T) {
+  buf := &bytes.Buffer{}
+  handler := weblogs.HandlerWithOptions(
+      &handler{Status: 321, LogExtra: "behere"}, &weblogs.Options{Writer: buf})
+  handler.ServeHTTP(
+      kNilResponseWriter,
+      newRequest("192.168.5.1", "GET", "/foo/bar?query=tall"))
+  actual := buf.String()
+  expected := "192.168.5.1 GET /foo/bar?query=tall 321 behere\n"
+  verifyLogs(t, expected, actual)
+}
+
+func TestSend500OnNoOutput(t *testing.T) {
+  buf := &bytes.Buffer{}
+  handler := weblogs.HandlerWithOptions(
+      &handler{LogExtra: "behere"}, &weblogs.Options{Writer: buf})
+  w := &spyResponseWriter{}
+  handler.ServeHTTP(
+      w,
+      newRequest("192.168.5.1", "GET", "/foo/bar?query=tall"))
+  actual := buf.String()
+  expected := "192.168.5.1 GET /foo/bar?query=tall 500 behere\n"
+  verifyLogs(t, expected, actual)
+  if w.Status != 500 {
+    t.Errorf("Expected 500 error to be sent, but %d was sent.", w.Status)
+  }
+}
+
+func TestUnwrappedCallToWriter(t *testing.T) {
+  // logging extra should should be silently ignored.
+  handler := &handler{LogExtra: "behere"}
+  handler.ServeHTTP(
+      kNilResponseWriter,
+      newRequest("192.168.5.1", "GET", "/foo/bar?query=tall"))
+}
+
+func verifyLogs(t *testing.T, expected, actual string) {
+  if expected == actual || !strings.HasSuffix(actual, expected) {
+    t.Errorf("Expected %s to be at end of %s", expected, actual)
+  }
+}
+
+func newRequest(remoteAddr, method, urlStr string) *http.Request {
+  u, _ := url.Parse(urlStr)
+  return &http.Request{
+    RemoteAddr: remoteAddr,
+    Method: method,
+    URL: u}
+}
+
+type handler struct {
+  Status int
+  LogExtra string
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  if h.Status != 0 {
+    http.Error(w, "Error", h.Status)
+  }
+  if h.LogExtra != "" {
+    fmt.Fprintf(weblogs.Writer(r), " %s", h.LogExtra)
+  }
+}
+
+type nilResponseWriter struct {
+}
+
+func (w nilResponseWriter) Write(b []byte) (n int, err error) {
+  return len(b), nil
+}
+
+func (w nilResponseWriter) WriteHeader(status int) {
+}
+
+func (w nilResponseWriter) Header() http.Header {
+  return http.Header{}
+}
+
+type spyResponseWriter struct {
+  nilResponseWriter
+  Status int
+}
+
+func (w *spyResponseWriter) WriteHeader(status int) {
+  w.Status = status
+}
+  
